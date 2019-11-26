3ee0e4735a66d95bc96c5fa5a031202f0cad078d
Xavier Puspus <xpuspus@gmail.com>
2019-11-26 11:56:19 +0800

    Added image recognition scripts

---
+++ app.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import matplotlib.image as mpimg
+from imageai.Prediction import ImagePrediction
+import os
+
+# Get path
+execution_path = '/Users/xmpuspus/Desktop/Lectures/notebooks/image_recog_app'
+image_path = '/Users/xmpuspus/Desktop/Lectures/notebooks/image_recog_app/images/'
+model_path = '/Users/xmpuspus/Desktop/Lectures/notebooks/image_recog_app/model/'
+model = '/Users/xmpuspus/Desktop/Lectures/notebooks/image_recog_app/model/resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+
+# os.chdir(execution_path)
+
+# Actual Prediction on image using Resnet
+prediction = ImagePrediction()
+st.cache(prediction.setModelTypeAsResNet())
+
+# model = 'model/resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+
+# Set Title
+st.title("Image Recognition")
+
+# Set Sidebar Options
+st.sidebar.title('About')
+st.sidebar.info('Choose an image to test the ResNet50 classifier.')
+st.sidebar.title("Predict New Images")
+onlyfiles = [f for f in os.listdir(image_path)]
+
+# Select image path from options
+imageselect = st.sidebar.selectbox("Pick an image.", onlyfiles)
+
+# Read image metadata
+img = mpimg.imread(os.path.join(image_path, imageselect))
+
+# Plot on streamlift
+st.image(img, caption="Let's predict the image!", use_column_width=True)
+
+if st.sidebar.button('Predict Image'):
+    
+#     model = 'model/resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+
+    st.cache(prediction.setModelPath(model))
+    st.cache(prediction.loadModel())
+    
+    # Get predictions
+    predictions, probabilities = prediction.predictImage(os.path.join(image_path, imageselect), result_count=5)
+
+    # Print out predictions
+    for eachPrediction, eachProbability in zip(predictions, probabilities):
+        st.write(eachPrediction , " : " , eachProbability)
